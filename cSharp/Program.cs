f77a09e49ffd18a9f50e076d5cf8779268d75fbe
tuyojr <olutuyod@gmail.com>
2022-08-02 18:39:05 +0100

    first commit

---
+++ cSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "\"Tuyo\"";
+            string phrase1 = "My name is Adedolapo ";
+            string phrase2 = " Olutuyo.";
+
+            Console.WriteLine(phrase1 + name + phrase2);
+
+            Console.WriteLine(phrase2.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(phrase1.Contains("name"));
+            Console.WriteLine(phrase1.Substring(11, 3));
+            Console.WriteLine(phrase1.Length);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
