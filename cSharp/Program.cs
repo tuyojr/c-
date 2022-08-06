4031f9b4e59c6cdfd3d1e4912c63d6e5b52d6a49
tuyojr <olutuyod@gmail.com>
2022-08-06 09:13:33 +0100

    Getting User Input

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -24,25 +24,39 @@ namespace cSharp
             //Console.WriteLine(phrase1.Substring(11, 3));
             //Console.WriteLine(phrase1.Length);
 
-            //2. Working with numbers
+            ////2. Working with numbers
 
-            int num = 4;
-            num++; //adds 1 to the num variable
-            //num-- subtracts 1 from the num variable
+            //int num = 4;
+            //num++; //adds 1 to the num variable
+            ////num-- subtracts 1 from the num variable
 
-            Console.WriteLine(num);
-            Console.WriteLine( 4 % 2 );
-            Console.WriteLine( (4 + 4) * 3 );
-            Console.WriteLine( 10 / 3 );
-            Console.WriteLine(10 / 3.0);
-            Console.WriteLine( Math.Abs(-300) ); //prints the absolute value to the screen
-            Console.WriteLine( Math.Pow(2, 3) ); //second number is the power which the first is raised to
-            Console.WriteLine( Math.Sqrt(64) ); //prints the square root of 64
-            Console.WriteLine( Math.Max( 10, 100 )); //prints out the greater number
-            Console.WriteLine( Math.Min( 10, 100 )); //prints out the lesser number
-            Console.WriteLine( Math.Round( 7.3 )); //rounds up the number to the nearest whole number. below .5 returns the number before the decimal point
-            Console.WriteLine( Math.Round( 7.5 )); //rounds up the number to the nearest whole number. .5 and above adds 1 to the number before the decimal point
-            Console.WriteLine(Convert.ToString(num));
+            //Console.WriteLine(num);
+            //Console.WriteLine( 4 % 2 );
+            //Console.WriteLine( (4 + 4) * 3 );
+            //Console.WriteLine( 10 / 3 );
+            //Console.WriteLine(10 / 3.0);
+            //Console.WriteLine( Math.Abs(-300) ); //prints the absolute value to the screen
+            //Console.WriteLine( Math.Pow(2, 3) ); //second number is the power which the first is raised to
+            //Console.WriteLine( Math.Sqrt(64) ); //prints the square root of 64
+            //Console.WriteLine( Math.Max( 10, 100 )); //prints out the greater number
+            //Console.WriteLine( Math.Min( 10, 100 )); //prints out the lesser number
+            //Console.WriteLine( Math.Round( 7.3 )); //rounds up the number to the nearest whole number. below .5 returns the number before the decimal point
+            //Console.WriteLine( Math.Round( 7.5 )); //rounds up the number to the nearest whole number. .5 and above adds 1 to the number before the decimal point
+            //Console.WriteLine(Convert.ToString(num));
+
+
+
+            //3. Getting User Input
+            //Console.WriteLine(); prints a line of text and then goes to a new line
+            //Console.ReaLine(); takes the line of text the user enters and saves it as a string
+            
+            
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", you are " + age + "years old.");
+
 
 
 
